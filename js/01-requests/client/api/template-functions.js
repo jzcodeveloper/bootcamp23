77c4dc38773493c1802ce1a4fe44ccb89ad07001
Javier Zabala <javier.zabala@javier.zabala>
2023-07-03 13:48:02 -0400

    refactor: js requests api

--- js/01-requests/client/api/template-functions.js
+++ js/01-requests/client/api/template-functions.js
@@ -5,32 +5,40 @@ const baseUrl = "http://localhost:3000/users";
  * @route /users
  * @method GET
  */
-export const getAll = async () => {};
+const getAll = async () => {};
 
 /**
  * @desc Gey by id
  * @route /users/:id
  * @method GET
  */
-export const getById = async (id) => {};
+const getById = async (id) => {};
 
 /**
  * @desc Creates a user
  * @route /users
  * @method POST
  */
-export const create = async (payload) => {};
+const create = async (payload) => {};
 
 /**
  * @desc Updates a user
  * @route /users/:id
  * @method PUT
  */
-export const update = async (id, payload) => {};
+const update = async (id, payload) => {};
 
 /**
  * @desc Removes a user
  * @route /users/:id
  * @method DELETE
  */
-export const remove = async () => {};
+const remove = async () => {};
+
+export default {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+};
